9bf66d27d8dca98a00474d36c4a9346f9fca04f1
andredev1 <andredev1234567@gmail.com>
2019-01-02 16:36:46 +0200

    project backed up

---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dVchocDataSet = new WindowsFormsApp4.DVchocDataSet();
+            this.tblClockInBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_ClockInTableAdapter = new WindowsFormsApp4.DVchocDataSetTableAdapters.tbl_ClockInTableAdapter();
+            this.fldidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldfirstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldlastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldpersonalIDnumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldosNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldosVersionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldbrowserNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldbrowserVersionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldnavigatorUserAgentDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldnavigatorAppVersionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldnavigatorPlatformDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldnavigatorVendorDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldlatitubeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fldlongitudeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.flddateTimeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dVchocDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblClockInBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.fldidDataGridViewTextBoxColumn,
+            this.fldfirstNameDataGridViewTextBoxColumn,
+            this.fldlastNameDataGridViewTextBoxColumn,
+            this.fldpersonalIDnumberDataGridViewTextBoxColumn,
+            this.fldosNameDataGridViewTextBoxColumn,
+            this.fldosVersionDataGridViewTextBoxColumn,
+            this.fldbrowserNameDataGridViewTextBoxColumn,
+            this.fldbrowserVersionDataGridViewTextBoxColumn,
+            this.fldnavigatorUserAgentDataGridViewTextBoxColumn,
+            this.fldnavigatorAppVersionDataGridViewTextBoxColumn,
+            this.fldnavigatorPlatformDataGridViewTextBoxColumn,
+            this.fldnavigatorVendorDataGridViewTextBoxColumn,
+            this.fldlatitubeDataGridViewTextBoxColumn,
+            this.fldlongitudeDataGridViewTextBoxColumn,
+            this.flddateTimeDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tblClockInBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 380);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(173, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Make an excel file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Clock In:";
+            // 
+            // dVchocDataSet
+            // 
+            this.dVchocDataSet.DataSetName = "DVchocDataSet";
+            this.dVchocDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblClockInBindingSource
+            // 
+            this.tblClockInBindingSource.DataMember = "tbl_ClockIn";
+            this.tblClockInBindingSource.DataSource = this.dVchocDataSet;
+            // 
+            // tbl_ClockInTableAdapter
+            // 
+            this.tbl_ClockInTableAdapter.ClearBeforeFill = true;
+            // 
+            // fldidDataGridViewTextBoxColumn
+            // 
+            this.fldidDataGridViewTextBoxColumn.DataPropertyName = "fld_id";
+            this.fldidDataGridViewTextBoxColumn.HeaderText = "fld_id";
+            this.fldidDataGridViewTextBoxColumn.Name = "fldidDataGridViewTextBoxColumn";
+            this.fldidDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // fldfirstNameDataGridViewTextBoxColumn
+            // 
+            this.fldfirstNameDataGridViewTextBoxColumn.DataPropertyName = "fld_firstName";
+            this.fldfirstNameDataGridViewTextBoxColumn.HeaderText = "fld_firstName";
+            this.fldfirstNameDataGridViewTextBoxColumn.Name = "fldfirstNameDataGridViewTextBoxColumn";
+            // 
+            // fldlastNameDataGridViewTextBoxColumn
+            // 
+            this.fldlastNameDataGridViewTextBoxColumn.DataPropertyName = "fld_lastName";
+            this.fldlastNameDataGridViewTextBoxColumn.HeaderText = "fld_lastName";
+            this.fldlastNameDataGridViewTextBoxColumn.Name = "fldlastNameDataGridViewTextBoxColumn";
+            // 
+            // fldpersonalIDnumberDataGridViewTextBoxColumn
+            // 
+            this.fldpersonalIDnumberDataGridViewTextBoxColumn.DataPropertyName = "fld_personalIDnumber";
+            this.fldpersonalIDnumberDataGridViewTextBoxColumn.HeaderText = "fld_personalIDnumber";
+            this.fldpersonalIDnumberDataGridViewTextBoxColumn.Name = "fldpersonalIDnumberDataGridViewTextBoxColumn";
+            // 
+            // fldosNameDataGridViewTextBoxColumn
+            // 
+            this.fldosNameDataGridViewTextBoxColumn.DataPropertyName = "fld_osName";
+            this.fldosNameDataGridViewTextBoxColumn.HeaderText = "fld_osName";
+            this.fldosNameDataGridViewTextBoxColumn.Name = "fldosNameDataGridViewTextBoxColumn";
+            // 
+            // fldosVersionDataGridViewTextBoxColumn
+            // 
+            this.fldosVersionDataGridViewTextBoxColumn.DataPropertyName = "fld_osVersion";
+            this.fldosVersionDataGridViewTextBoxColumn.HeaderText = "fld_osVersion";
+            this.fldosVersionDataGridViewTextBoxColumn.Name = "fldosVersionDataGridViewTextBoxColumn";
+            // 
+            // fldbrowserNameDataGridViewTextBoxColumn
+            // 
+            this.fldbrowserNameDataGridViewTextBoxColumn.DataPropertyName = "fld_browserName";
+            this.fldbrowserNameDataGridViewTextBoxColumn.HeaderText = "fld_browserName";
+            this.fldbrowserNameDataGridViewTextBoxColumn.Name = "fldbrowserNameDataGridViewTextBoxColumn";
+            // 
+            // fldbrowserVersionDataGridViewTextBoxColumn
+            // 
+            this.fldbrowserVersionDataGridViewTextBoxColumn.DataPropertyName = "fld_browserVersion";
+            this.fldbrowserVersionDataGridViewTextBoxColumn.HeaderText = "fld_browserVersion";
+            this.fldbrowserVersionDataGridViewTextBoxColumn.Name = "fldbrowserVersionDataGridViewTextBoxColumn";
+            // 
+            // fldnavigatorUserAgentDataGridViewTextBoxColumn
+            // 
+            this.fldnavigatorUserAgentDataGridViewTextBoxColumn.DataPropertyName = "fld_navigatorUserAgent";
+            this.fldnavigatorUserAgentDataGridViewTextBoxColumn.HeaderText = "fld_navigatorUserAgent";
+            this.fldnavigatorUserAgentDataGridViewTextBoxColumn.Name = "fldnavigatorUserAgentDataGridViewTextBoxColumn";
+            // 
+            // fldnavigatorAppVersionDataGridViewTextBoxColumn
+            // 
+            this.fldnavigatorAppVersionDataGridViewTextBoxColumn.DataPropertyName = "fld_navigatorAppVersion";
+            this.fldnavigatorAppVersionDataGridViewTextBoxColumn.HeaderText = "fld_navigatorAppVersion";
+            this.fldnavigatorAppVersionDataGridViewTextBoxColumn.Name = "fldnavigatorAppVersionDataGridViewTextBoxColumn";
+            // 
+            // fldnavigatorPlatformDataGridViewTextBoxColumn
+            // 
+            this.fldnavigatorPlatformDataGridViewTextBoxColumn.DataPropertyName = "fld_navigatorPlatform";
+            this.fldnavigatorPlatformDataGridViewTextBoxColumn.HeaderText = "fld_navigatorPlatform";
+            this.fldnavigatorPlatformDataGridViewTextBoxColumn.Name = "fldnavigatorPlatformDataGridViewTextBoxColumn";
+            // 
+            // fldnavigatorVendorDataGridViewTextBoxColumn
+            // 
+            this.fldnavigatorVendorDataGridViewTextBoxColumn.DataPropertyName = "fld_navigatorVendor";
+            this.fldnavigatorVendorDataGridViewTextBoxColumn.HeaderText = "fld_navigatorVendor";
+            this.fldnavigatorVendorDataGridViewTextBoxColumn.Name = "fldnavigatorVendorDataGridViewTextBoxColumn";
+            // 
+            // fldlatitubeDataGridViewTextBoxColumn
+            // 
+            this.fldlatitubeDataGridViewTextBoxColumn.DataPropertyName = "fld_latitube";
+            this.fldlatitubeDataGridViewTextBoxColumn.HeaderText = "fld_latitube";
+            this.fldlatitubeDataGridViewTextBoxColumn.Name = "fldlatitubeDataGridViewTextBoxColumn";
+            // 
+            // fldlongitudeDataGridViewTextBoxColumn
+            // 
+            this.fldlongitudeDataGridViewTextBoxColumn.DataPropertyName = "fld_longitude";
+            this.fldlongitudeDataGridViewTextBoxColumn.HeaderText = "fld_longitude";
+            this.fldlongitudeDataGridViewTextBoxColumn.Name = "fldlongitudeDataGridViewTextBoxColumn";
+            // 
+            // flddateTimeDataGridViewTextBoxColumn
+            // 
+            this.flddateTimeDataGridViewTextBoxColumn.DataPropertyName = "fld_dateTime";
+            this.flddateTimeDataGridViewTextBoxColumn.HeaderText = "fld_dateTime";
+            this.flddateTimeDataGridViewTextBoxColumn.Name = "flddateTimeDataGridViewTextBoxColumn";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dVchocDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblClockInBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private DVchocDataSet dVchocDataSet;
+        private System.Windows.Forms.BindingSource tblClockInBindingSource;
+        private DVchocDataSetTableAdapters.tbl_ClockInTableAdapter tbl_ClockInTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldfirstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldlastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldpersonalIDnumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldosNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldosVersionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldbrowserNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldbrowserVersionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldnavigatorUserAgentDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldnavigatorAppVersionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldnavigatorPlatformDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldnavigatorVendorDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldlatitubeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fldlongitudeDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn flddateTimeDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ WindowsFormsApp4/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,92 @@
+﻿using ExcelLibrary.SpreadSheet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'dVchocDataSet.tbl_ClockIn' table. You can move, or remove it, as needed.
+            this.tbl_ClockInTableAdapter.Fill(this.dVchocDataSet.tbl_ClockIn);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string file = ("ClockIn_Report " + System.DateTime.Now.ToString("yyyy-MM-dd HHTmm") + ".xls");
+            Workbook workbook = new Workbook();
+            Worksheet worksheet = new Worksheet("First Sheet");
+            worksheet.Cells[0, 0] = new Cell("fld_id");
+            worksheet.Cells[0, 1] = new Cell("fld_firstName");
+            worksheet.Cells[0, 2] = new Cell("fld_lastName");
+            worksheet.Cells[0, 3] = new Cell("fld_personalIDnumber");
+            worksheet.Cells[0, 4] = new Cell("fld_osName");
+            worksheet.Cells[0, 5] = new Cell("fld_osVersion");
+            worksheet.Cells[0, 6] = new Cell("fld_browserName");
+            worksheet.Cells[0, 7] = new Cell("fld_browserVersion");
+            worksheet.Cells[0, 8] = new Cell("fld_navigatorUserAgent");
+            worksheet.Cells[0, 9] = new Cell("fld_navigatorAppVersion");
+            worksheet.Cells[0, 10] = new Cell("fld_navigatorPlatform");
+            worksheet.Cells[0, 11] = new Cell("fld_navigatorVendor");
+            worksheet.Cells[0, 12] = new Cell("fld_latitube");
+            worksheet.Cells[0, 13] = new Cell("fld_longitude");
+            worksheet.Cells[0, 14] = new Cell("fld_dateTime");
+            
+            List<string[]> queryResult = new back.SQLConnect().GetClassList();
+
+            worksheet.Cells[1, 0] = new Cell(queryResult[0].ToArray()[0]);
+            worksheet.Cells[1, 1] = new Cell(queryResult[0].ToArray()[1]);
+            worksheet.Cells[1, 2] = new Cell(queryResult[0].ToArray()[2]);
+            worksheet.Cells[1, 3] = new Cell(queryResult[0].ToArray()[3]);
+            worksheet.Cells[1, 4] = new Cell(queryResult[0].ToArray()[4]);
+            worksheet.Cells[1, 5] = new Cell(queryResult[0].ToArray()[5]);
+            worksheet.Cells[1, 6] = new Cell(queryResult[0].ToArray()[6]);
+            worksheet.Cells[1, 7] = new Cell(queryResult[0].ToArray()[7]);
+            worksheet.Cells[1, 8] = new Cell(queryResult[0].ToArray()[8]);
+            worksheet.Cells[1, 9] = new Cell(queryResult[0].ToArray()[9]);
+            worksheet.Cells[1, 10] = new Cell(queryResult[0].ToArray()[10]);
+            worksheet.Cells[1, 11] = new Cell(queryResult[0].ToArray()[11]);
+            worksheet.Cells[1, 12] = new Cell(queryResult[0].ToArray()[12]);
+            worksheet.Cells[1, 13] = new Cell(queryResult[0].ToArray()[13]);
+            worksheet.Cells[1, 14] = new Cell(queryResult[0].ToArray()[14]);
+
+
+
+            workbook.Worksheets.Add(worksheet);
+            workbook.Save(file);
+
+            Workbook book = Workbook.Load(file); Worksheet sheet = book.Worksheets[0];
+
+            //foreach (Pair, Cell> cell in sheet.Cells) 
+            //{
+            //    dgvCells[cell.Left.Right, cell.Left.Left].Value = cell.Right.Value;
+            //}
+
+            for (int rowIndex = sheet.Cells.FirstRowIndex; rowIndex <= sheet.Cells.LastRowIndex; rowIndex++)
+            {
+                Row row = sheet.Cells.GetRow(rowIndex);
+                for (int colIndex = row.FirstColIndex; colIndex <= row.LastColIndex; colIndex++)
+                {
+                    Cell cell = row.GetCell(colIndex);
+                }
+            }
+
+            MessageBox.Show("File Created");
+
+        }
+    }
+}
